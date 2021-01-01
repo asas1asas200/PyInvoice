cb4b73e68ade4dfa8e8a308b32677bd5fb1584c1
asas1asas200 <asas1asas200@gmail.com>
2021-01-02 01:50:13 +0800

    Normalize address field of invoices

--- src/Crawler.py
+++ src/Crawler.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from time import time
 from threading import Thread
 from queue import Queue
-from collections import namedtuple
+from collections import namedtuple, Counter
 class Crawler:
 	def __init__(self):
 		self.Invoice = namedtuple('Invoice', ['com', 'addr', 'items'])
@@ -14,16 +14,24 @@ class Crawler:
 			return soup
 		def get_invoices(url, q):
 			soup = parse_page(url)
-			thousand = [ self.Invoice(*[ j.text for j in i.select('td')[-3:] ])
-				for i in soup.find(id='fbonly').select('tr')[1:] ]
-			two_hundred = [ self.Invoice(*[ j.text for j in i.select('td')[-3:] ])
-				for i in soup.find(id='fbonly_200').select('tr')[1:] ]
-			q.put((thousand, two_hundred))
+			date = soup.find('a', {'data-toggle': 'tab'}).text[:-15]
+			thousand = []
+			two_hundred = []
+			for html_id, price in (('fbonly', thousand), ('fbonly_200', two_hundred)): # 1000W and 200W
+				for invoice in soup.find(id=html_id).select('tr')[1:]:
+					com, addr, items = [i.text for i in invoice.select('td')[-3:]]
+					addr = addr[:3]
+					if addr == '台北市':
+						addr = '臺北市'
+					elif addr == '桃園縣':
+						addr = '桃園市'
+					price.append(self.Invoice(com, addr, items))
+
+			q.put({date: {'thousand': thousand, 'two_hundred': two_hundred}})
 			
 		self.home = 'https://www.etax.nat.gov.tw/'
 		soup = parse_page('https://www.etax.nat.gov.tw/etw-main/web/ETW183W1/')
 		lst = soup.find_all('td', {'headers': 'title'})[:-8:2]
-		s = time()
 		q = Queue()
 		threads = []
 		for i in lst:
@@ -32,15 +40,23 @@ class Crawler:
 			threads.append(t)
 		for i in threads:
 			i.join()
-		res = []
+		self.invoices = {}
 		while q.qsize():
-			print(q.get())
-#			r = requests.get(self.home + i.find('a')['href'])
-#			r.encoding = 'utf-8'
-#			print(r.text)
-		print(time() - s)
+			self.invoices.update(q.get())
+
+	@property
+	def dates(self):
+		return sorted(self.invoices.keys(), reverse=True)
+
+	@property
+	def addrs(self):
+		ret = []
+		for i in self.invoices.values():
+			for price in ('thousand', 'two_hundred'):
+				for j in i[price]:
+					ret.append(j.addr)
+		return sorted(ret)
 	
 if __name__ == '__main__':
 	crawler = Crawler()
-	
-		
+	print(Counter(crawler.addrs))
