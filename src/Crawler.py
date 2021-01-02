5e3350accd8dc2f58c65e8af3c1fe7a23abcc4a7
asas1asas200 <asas1asas200@gmail.com>
2021-01-02 16:48:49 +0800

    Create and Normalize spent field of invoices

--- src/Crawler.py
+++ src/Crawler.py
@@ -4,15 +4,30 @@ from time import time
 from threading import Thread
 from queue import Queue
 from collections import namedtuple, Counter
+import re
 class Crawler:
 	def __init__(self):
-		self.Invoice = namedtuple('Invoice', ['com', 'addr', 'items'])
+		self.Invoice = namedtuple('Invoice', ['com', 'addr', 'items', 'spent'])
 		def parse_page(url):
 			r = requests.get(url)
 			r.encoding = 'utf-8'
 			soup = BeautifulSoup(r.text, 'html.parser')
 			return soup
 		def get_invoices(url, q):
+			def parse_addr(addr):
+				addr = addr[:3]
+				if addr == '台北市':
+					addr = '臺北市'
+				elif addr == '桃園縣':
+					addr = '桃園市'
+				return addr
+
+			def parse_items(items):
+				items = re.sub(r'[ ,]', '', items)
+				match = re.search(r'[共計]?(\d*)元', items)
+				spent = int(match[1]) if match else -1
+				return items, spent
+		
 			soup = parse_page(url)
 			date = soup.find('a', {'data-toggle': 'tab'}).text[:-15]
 			thousand = []
@@ -20,12 +35,9 @@ class Crawler:
 			for html_id, price in (('fbonly', thousand), ('fbonly_200', two_hundred)): # 1000W and 200W
 				for invoice in soup.find(id=html_id).select('tr')[1:]:
 					com, addr, items = [i.text for i in invoice.select('td')[-3:]]
-					addr = addr[:3]
-					if addr == '台北市':
-						addr = '臺北市'
-					elif addr == '桃園縣':
-						addr = '桃園市'
-					price.append(self.Invoice(com, addr, items))
+					addr = parse_addr(addr)
+					items, spent = parse_items(items)
+					price.append(self.Invoice(com, addr, items, spent))
 
 			q.put({date: {'thousand': thousand, 'two_hundred': two_hundred}})
 			
@@ -48,15 +60,27 @@ class Crawler:
 	def dates(self):
 		return sorted(self.invoices.keys(), reverse=True)
 
-	@property
-	def addrs(self):
+	def __get_property(self, attr):
 		ret = []
 		for i in self.invoices.values():
 			for price in ('thousand', 'two_hundred'):
 				for j in i[price]:
-					ret.append(j.addr)
+					ret.append(getattr(j, attr))
 		return sorted(ret)
-	
+	@property
+	def spents(self):
+		return self.__get_property('spent')
+
+	@property
+	def items(self):
+		return self.__get_property('items')
+
+	@property
+	def addrs(self):
+		return self.__get_property('addr')
+
 if __name__ == '__main__':
 	crawler = Crawler()
-	print(Counter(crawler.addrs))
+	with open('spents.txt', 'w') as file:
+		file.write('\n'.join(map(str,crawler.spents)))
+		
