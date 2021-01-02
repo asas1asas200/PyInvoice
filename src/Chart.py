0b2538a5da1ba83ec2832061d220b495982c5510
asas1asas200 <asas1asas200@gmail.com>
2021-01-02 20:52:55 +0800

    Initial Chart and add font file

---
+++ src/Chart.py
@@ -0,0 +1,11 @@
+from matplotlib.font_manager import FontProperties
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.figure as Figure
+from os import path
+
+class Chart:
+	def __init__(self):
+		pass
+
+if __name__ == '__main__':
+	chart = Chart()
